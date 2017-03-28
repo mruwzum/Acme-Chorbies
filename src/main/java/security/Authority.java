37bcce3e45613a281b06239c03b93bef94a8a86a
mruwzum <daviddelatorre00@gmail.com>
2017-03-28 17:04:45 +0200

    added credentials authorities

--- src/main/java/security/Authority.java
+++ src/main/java/security/Authority.java
@@ -37,8 +37,8 @@ public class Authority implements GrantedAuthority {
 
 	// Values -----------------------------------------------------------------
 
-	public static final String	ADMIN		= "ADMIN";
-	public static final String	CUSTOMER	= "CUSTOMER";
+	public static final String	ADMINISTRATOR		= "ADMINISTRATOR";
+	public static final String	CHORBI	= "CHORBI";
 
 	// Attributes -------------------------------------------------------------
 
@@ -46,7 +46,7 @@ public class Authority implements GrantedAuthority {
 
 
 	@NotBlank
-	@Pattern(regexp = "^" + Authority.ADMIN + "|" + Authority.CUSTOMER + "$")
+	@Pattern(regexp = "^" + Authority.ADMINISTRATOR + "|" + Authority.CHORBI + "$")
 	@Override
 	public String getAuthority() {
 		return this.authority;
@@ -63,11 +63,11 @@ public class Authority implements GrantedAuthority {
 		result = new ArrayList<Authority>();
 
 		authority = new Authority();
-		authority.setAuthority(Authority.ADMIN);
+		authority.setAuthority(Authority.ADMINISTRATOR);
 		result.add(authority);
 
 		authority = new Authority();
-		authority.setAuthority(Authority.CUSTOMER);
+		authority.setAuthority(Authority.CHORBI);
 		result.add(authority);
 
 		return result;
