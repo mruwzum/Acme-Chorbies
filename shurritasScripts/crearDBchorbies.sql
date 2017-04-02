01478e872c53ac052beae12a5f8d75042d80d707
mruwzum <daviddelatorre00@gmail.com>
2017-04-02 13:31:05 +0200

    everything allright

--- shurritasScripts/crearDBchorbies.sql
+++ shurritasScripts/crearDBchorbies.sql
@@ -4,11 +4,11 @@ drop user 'acme-user'@'%';
 drop user 'acme-manager'@'%';
 create user 'acme-user'@'%' identified by password '*4F10007AADA9EE3DBB2CC36575DFC6F4FDE27577';
 create user 'acme-manager'@'%' identified by password '*FDB8CD304EB2317D10C95D797A4BD7492560F55F';
-grant select, insert, update, delete 
+grant select, insert, update, delete
 	on `Acme-Chorbies`.* to 'acme-user'@'%';
 
-grant select, insert, update, delete, create, drop, references, index, alter, 
-        create temporary tables, lock tables, create view, create routine, 
+grant select, insert, update, delete, create, drop, references, index, alter,
+        create temporary tables, lock tables, create view, create routine,
         alter routine, execute, trigger, show view
     on `Acme-Chorbies`.* to 'acme-manager'@'%';
 
