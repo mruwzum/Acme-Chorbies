5b2edab217e17348b8d406d9fb847bd6fc429b88
David De la torre <daviddelatorre@MacBook-Air-de-David.local>
2017-04-07 16:38:16 +0200

    queries

--- src/main/java/repositories/SearchRepository.java
+++ src/main/java/repositories/SearchRepository.java
@@ -20,4 +20,33 @@ public interface SearchRepository extends JpaRepository<Search, Integer> {
 
     @Query("select c from Chorbi c where c.age between ?1 and ?4 and c.genre=?2 and c.relationship=?3")
     List<Chorbi> finderQuery2(int underage, Genre genre, Relationship relationship, int overage);
+
+
+
+
+    @Query("select c from Chorbi c where c.age between ?1 and ?4")
+    List<Chorbi> findByAge(int underage, int overage);
+
+    @Query("select c from Chorbi c where c.genre=?1")
+    List<Chorbi> findByGenre(Genre genre);
+
+    @Query("select c from Chorbi c where c.relationship=?1")
+    List<Chorbi> findByRelationship(Relationship relationship);
+
+    @Query("select c from Chorbi c where c.description like %?1")
+    List<Chorbi> findByKeyword(String keyword);
+
+    @Query("select c from Chorbi c where c.coordinate.city = ?1")
+    List<Chorbi> findByCity(String city);
+
+    @Query("select c from Chorbi c where c.coordinate.state = ?1")
+    List<Chorbi> findByState(String state);
+
+    @Query("select c from Chorbi c where c.coordinate.country = ?1")
+    List<Chorbi> findByCountry(String country);
+
+    @Query("select c from Chorbi c where c.coordinate.province = ?1")
+    List<Chorbi> findByProvince(String province);
+
+
 }
